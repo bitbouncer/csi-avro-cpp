b4ad1f757fef59ce2cb269f06996da42e5417f08
svante karlsson <svante.karlsson@csi.se>
2015-07-09 22:54:19 +0200

    renamed function

--- csi_avro/encoding.h
+++ csi_avro/encoding.h
@@ -114,7 +114,7 @@ namespace csi
     bool avro_binary_decode_with_fingerprint(const avro::OutputStream& src, T& dst)
     {
         auto mis = avro::memoryInputStream(src);
-        return avro_binary_decode_with_schema(*mis);
+        return avro_binary_decode_with_fingerprint(*mis);
     }
 
     template<class T>
